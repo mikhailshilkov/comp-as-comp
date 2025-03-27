e951c75f0e6d4fce2049451a52c08dc1f42c059a
Julien <julien@caffeine.lu>
2025-03-27 16:45:42 +0100

    Update Python example for 3.159.0 SDK

--- py/__main__.py
+++ py/__main__.py
@@ -1,5 +1,5 @@
-from pulumi.provider.experimental import Metadata, component_provider_host
+from pulumi.provider.experimental import component_provider_host
 from staticpage import StaticPage
 
 if __name__ == "__main__":
-    component_provider_host(metadata=Metadata("python-components"), components=[StaticPage])
+    component_provider_host(name="python-components", components=[StaticPage])
