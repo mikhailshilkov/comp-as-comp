f3848d1e7bd9e16b83b9f9e99b45a305817e922d
Eron Wright <eron@pulumi.com>
2025-04-04 08:01:28 -0700

    remove dead comments

--- apps/super-random-ts/generate.sh
+++ apps/super-random-ts/generate.sh
@@ -1,9 +1,6 @@
 set -o errexit
 set -o pipefail
 
-PULUMI_BUILD_CONTAINER_VERSION=v0.3.0
-
-# First build the image for the Pulumi Build Container
 echo "* Generating go-components SDK:"
 pulumi package add ../../go
 
